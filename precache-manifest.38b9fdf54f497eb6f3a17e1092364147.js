8c774c7a835b39e6879dd9eb8202ead98b6bba3e
ZZZZeta <fastmuvie@gmail.com>
2019-09-08 12:03:20 +0300

    Updates

---
+++ precache-manifest.38b9fdf54f497eb6f3a17e1092364147.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2b87d1b54aeb1b617a42c8230d9fcbc",
+    "url": "/porfolio-my-site/index.html"
+  },
+  {
+    "revision": "eaf3b309cf9c54af261a",
+    "url": "/porfolio-my-site/static/js/2.0c734d74.chunk.js"
+  },
+  {
+    "revision": "345382a485f9d660e190",
+    "url": "/porfolio-my-site/static/js/main.bec6edc5.chunk.js"
+  },
+  {
+    "revision": "2b1ddff2b57bca906cbb",
+    "url": "/porfolio-my-site/static/js/runtime~main.cc6f7630.js"
+  },
+  {
+    "revision": "710589d49f56752ddcca0ba4bf416995",
+    "url": "/porfolio-my-site/static/media/burger-menu.710589d4.svg"
+  },
+  {
+    "revision": "aeec28632f4519bdb1c51013fead8ccd",
+    "url": "/porfolio-my-site/static/media/close-button.aeec2863.svg"
+  },
+  {
+    "revision": "4e6682858dcc327b4914abacf2efa9db",
+    "url": "/porfolio-my-site/static/media/design.4e668285.svg"
+  },
+  {
+    "revision": "583b16ebe1d12e41c68f1e7b1404113f",
+    "url": "/porfolio-my-site/static/media/developer.583b16eb.svg"
+  },
+  {
+    "revision": "87660ce770ad32c04dd7c3b951b0d36b",
+    "url": "/porfolio-my-site/static/media/main.87660ce7.svg"
+  },
+  {
+    "revision": "5ad535bf204413326cbd088f3ff932e2",
+    "url": "/porfolio-my-site/static/media/technology.5ad535bf.svg"
+  }
+]);
